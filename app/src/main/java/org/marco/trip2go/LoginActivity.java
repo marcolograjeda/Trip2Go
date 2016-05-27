6351f2ecd736655858dce6a5d88c50803d15b7ed
Marco <juniorlograjeda@gmail.com>
2016-05-27 09:05:36 -0600

    Login terminado

--- app/src/main/java/org/marco/trip2go/LoginActivity.java
+++ app/src/main/java/org/marco/trip2go/LoginActivity.java
@@ -2,12 +2,63 @@ package org.marco.trip2go;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+import org.marco.trip2go.Volley.WebService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView txtNick, txtContraseña;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin=(Button) findViewById(R.id.btnLogin);
+        txtNick=(TextView) findViewById(R.id.txtNick);
+        txtContraseña=(TextView) findViewById(R.id.txtContraseña);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Map<String, String>params=new HashMap<String, String>();
+                params.put("nick",txtNick.getText().toString());
+                params.put("contrasena",txtContraseña.getText().toString());
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, WebService.autenticar, new JSONObject(params), new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+                            /*if (response.toString().equals("Tu nickname o contraseña no son validos")){
+                                Toast.makeText(getApplicationContext(), "Nickname o contraseña no validos", Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_LONG).show();
+                            }*/
+                        }catch (Exception ex){
+                            Log.e("Response exeption ", ex.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Log.d("Error: Response ", error.getMessage());
+                    }
+                });
+                WebService.getInstance(v.getContext()).addToRequestQueue(request);
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/marco/trip2go/Volley/WebService.java
@@ -0,0 +1,66 @@
+package org.marco.trip2go.Volley;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by marco on 26/05/16.
+ */
+public class WebService {
+    //192.168.101.161
+    public static String server="http://192.168.101.161:3000/api/v1";
+    public static String autenticar=server+"/usuario/login";
+    private static WebService mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private WebService(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized WebService getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new WebService(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
